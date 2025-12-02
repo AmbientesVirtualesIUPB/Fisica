e8412c09ad2ca82ee9d7f6a9932d68cd0ec552f6
Morion <ambientesvirtuales@pascualbravo.edu.co>
2025-12-02 18:39:53 -0500

    Cielo, Trebuchet, perros animados, flechas que queman

---
+++ Assets/Fisica/Scripts/Parabolico/Chozas.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ControlQuemado))]
+[RequireComponent(typeof(Rigidbody))]
+public class Chozas : MonoBehaviour
+{
+    public ControlQuemado controlQuemado;
+
+    private void Awake()
+    {
+        controlQuemado = GetComponent<ControlQuemado>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Flecha"))
+        {
+            controlQuemado.EjecutarAnimacion();
+        }
+    }
+}
